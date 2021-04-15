da3242b6c23fcabac4fde2d2c4b74c5d6d67911c
xiaobing318 <1966630575@qq.com>
2021-04-16 00:08:26 +0800

    video07 series

---
+++ video07_Log.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "video07_Log.h"
+
+void Initial(const char* message)
+{
+	Log("Initial log");
+}
+
+
+
+/*
+	下面的这个函数是为了之前的解释
+*/
+//void Log(const char* message)
+//{
+//	std::cout << message << std::endl;
+//}
+
