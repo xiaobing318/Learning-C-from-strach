2f6ef80927b44d03776eed395edea785f763845d
xiaobing318 <1966630575@qq.com>
2021-04-19 15:53:04 +0800

    video10 series

---
+++ video10_Log.h
@@ -0,0 +1,39 @@
+
+//首先要说明的一件事情就是：pragma once的意思就是compile once
+
+/*
+1)说明
+	只要是#符号的，那么这个statement就是一个preprocessing statement，这就意味着compiler将会
+首先处理这个statement，然后才会处理file中的code，这便是这个preprocessing statement的作用。
+
+	其实最主要的作用就是为了防止多次被definition，为什么会有这种说法？因为首先我们要清楚的一件
+事情就是#include 代表的含义就是copy and paste，那么如果在多个文件中都包含了这个statement，那
+么在compile的时候是不会出现问题的，因为每一个cpp file都是单独的一个unit，但是在link成一个大的
+文件的时候将会发生不一样的事情：有些东西被copy and paste了很多次，也就是definition了很多次，那
+么在这种情况下，将会发生多次definition的事情发生。
+
+	从而这也就是#pragma once statement的意义所在。就是为了防止多次definition，它所做的工作就
+是只被definition一次。
+*/
+#pragma once
+
+/*
+	这里还有一件事情需要注意：#pragma once preprocessing statement 之前的写法是#ifndef and
+#define and so on.
+	但是这里我们还是继续使用这种更为简洁的写法，但是它们所达到的目的却都是相同的。
+*/
+
+
+void InitialLog();
+
+void Log(const char* message);
+
+struct Player
+{
+
+};
+
+/*
+	最为重要的一件事情就是要清楚#include具体做了什么样的工作，只有这样才能更好的理解c++的
+工作原理。
+*/
---
+++ video10_main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "video10_Log.h"
+#include "video10_Common.h"
+
+/*
+1)说明
+	关于这个#include "" and #include <>我专门search了一下，原来是两种不同的搜索路径，因为
+这两种header file存储在不同的位置，因此我们要做的一件事情就是将这两种对应的header按照一定的
+路径找出来。
+
+	#include ""是先搜索当前的文件夹所在的位置有没有对应的header文件，然后再按照标准的搜索路
+径进行寻找对应的文件。
+	#include <>则是直接按照标准的搜索路径进行搜索，这里所说的标准的搜索路径指的是已经设定好
+了的system file.
+
+2)为什么iostream没有extension？
+	为了回答这个问题，我们首先要知道：一般来说c++和c的一些标准库还是不太一样的，但是在c++中
+我们是可以使用c的库的，因此为了区分这两种库的不同，所以就产生了这两种不同的include 方式。
+*/
+
+
+//http://gcc.gnu.org/onlinedocs/gcc-2.95.3/cpp_1.html#SEC6
+//解释#include "" and #include <>的区别
+
+int main()
+{
+	Log("hello world");
+	InitialLog();
+	std::cin.get();
+}
